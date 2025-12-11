e01e38df6422ce3aa2349c891d262dc8df0a25e3
hasanaat <arsheema619@gmail.com>
2025-12-11 18:28:08 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import HostDashboard from "./pages/HostDashboard";
 import PublicBookingPage from "./pages/PublicBookingPage";
 import Register from "./pages/Register";
+
 const App = () => {
   return (
     <BrowserRouter>
