43c612cad592ef9f40769a25a69de807afa438cc
hasanaat <arsheema619@gmail.com>
2025-12-17 12:46:32 +0530

    slot-controllers/changes

--- backend/src/controllers/slot-controller.js
+++ backend/src/controllers/slot-controller.js
@@ -97,7 +97,6 @@ export const updateSlot = async (req, res) => {
     if (overlapping) {
       return res.status(400).json({ message: "This time overlaps with another slot." });
     }
-    
     slot.date = nextDate;
     slot.startTime = normalizedStart;
     slot.endTime = normalizedEnd;
