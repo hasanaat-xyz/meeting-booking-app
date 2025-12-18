24e62de63e2d2a6e35302bbd2cc432c933500654
hasanaat <arsheema619@gmail.com>
2025-12-18 09:14:50 +0530

    booking/model

--- backend/src/models/booking-model.js
+++ backend/src/models/booking-model.js
@@ -4,6 +4,7 @@ const bookingSchema = new mongoose.Schema({
   slotId: { type: mongoose.Schema.Types.ObjectId, ref: "Slot", unique: true },
   name: String,
   email: String,
+  
   purpose: String
 }, { timestamps: true });
 
