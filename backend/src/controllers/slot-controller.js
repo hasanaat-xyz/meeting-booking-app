4f4cdbf4cc8d2c871b14400b6e811359b4374daa
hasanaat <arsheema619@gmail.com>
2025-12-19 09:41:50 +0530

    model updated

--- backend/src/controllers/slot-controller.js
+++ backend/src/controllers/slot-controller.js
@@ -106,6 +106,7 @@ export const updateSlot = async (req, res) => {
     console.error("Update Slot Error:", error);
     res.status(500).json({ success: false, message: "Server error", error: error.message });
   }
+  
 };
 
 // Delete Slot
